71b727c3ed60420503ed257490489617289669a6
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2021-07-18 17:10:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newsthetic/DetailBeritaActivity.java
@@ -0,0 +1,14 @@
+package com.example.newsthetic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailBeritaActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail_berita);
+  }
+}
